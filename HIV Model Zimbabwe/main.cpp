a1ef8e54589c61e591998511f56650463ef65abc
Mikaela Smit <mikaela.smit@gmail.com>
2016-01-27 13:07:12 +0000

    Initial Commit

---
+++ HIV Model Zimbabwe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HIV Model Zimbabwe
+//
+//  Created by Mikaela Smit on 27/01/2016.
+//  Copyright © 2016 Mikaela Smit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
